ad7d72150c76577cb4fdef76335ad4e407646f02
Gustavo Farias <gustavojosefariasdesouza@gmail.com>
2025-09-30 08:59:49 -0300

    refactor: refatorando o valor da nota para grade_value

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,9 +12,9 @@ INSERT INTO exams (title, subject_id) VALUES ('History Exam 1', 2);
 INSERT INTO exams (title, subject_id) VALUES ('Science Exam 1', 3);
 INSERT INTO exams (title, subject_id) VALUES ('Math Exam 2', 1);
 
-INSERT INTO grades (student_id, exam_id, value) VALUES (1, 1, 9.5);
-INSERT INTO grades (student_id, exam_id, value) VALUES (2, 1, 8.0);
-INSERT INTO grades (student_id, exam_id, value) VALUES (3, 2, 7.5);
-INSERT INTO grades (student_id, exam_id, value) VALUES (1, 4, 10.0);
-INSERT INTO grades (student_id, exam_id, value) VALUES (2, 3, 9.0);
-INSERT INTO grades (student_id, exam_id, value) VALUES (3, 3, 8.5);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (1, 1, 9.5);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (2, 1, 8.0);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (3, 2, 7.5);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (1, 4, 10.0);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (2, 3, 9.0);
+INSERT INTO grades (student_id, exam_id, grade_value) VALUES (3, 3, 8.5);
